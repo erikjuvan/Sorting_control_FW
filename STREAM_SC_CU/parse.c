74fcc8db65e31d367b280f30d71390f5f982b732
erik.juvan <erik.kiefer.juvan@sensum.eu>
2019-03-19 18:08:04 +0100

    Added echo with all SET functions (for error control). Removed all newlines when sending text back.

--- STREAM_SC_CU/parse.c
+++ STREAM_SC_CU/parse.c
@@ -59,7 +59,7 @@ static void Function_UART_SORT(char* str, write_func Write)
 static void Function_VERG(char* str, write_func Write)
 {
     char buf[100] = {0};
-    snprintf(buf, sizeof(buf), "VERG,%s,%s,%s\n", SWVER, HWVER, COMPATIBILITYMODE);
+    snprintf(buf, sizeof(buf), "VERG,%s,%s,%s", SWVER, HWVER, COMPATIBILITYMODE);
     Write((uint8_t*)buf, strlen(buf));
 }
 
@@ -76,12 +76,12 @@ static void Function_VRBS(char* str, write_func Write)
 static void Function_VRBG(char* str, write_func Write)
 {
     char buf[10] = {0};
-    snprintf(buf, sizeof(buf), "VRBG,%u\n", g_verbose_level);
+    snprintf(buf, sizeof(buf), "VRBG,%u", g_verbose_level);
 
     Write((uint8_t*)buf, strlen(buf));
 }
 
-static void Function_IDST(char* str, write_func Write)
+static void Function_SETID(char* str, write_func Write)
 {
     str = strtok(NULL, Delims);
     if (str != NULL) {
@@ -90,13 +90,17 @@ static void Function_IDST(char* str, write_func Write)
             UART_Set_Address(num);
         }
     }
+
+    // Echo
+    char buf[10] = {0};
+    snprintf(buf, sizeof(buf), "SETID,%u", UART_Address);
+    Write((uint8_t*)buf, strlen(buf));
 }
 
-static void Function_IDGT(char* str, write_func Write)
+static void Function_GETID(char* str, write_func Write)
 {
     char buf[10] = {0};
-    snprintf(buf, sizeof(buf), "ID:%u\n", UART_Address);
-
+    snprintf(buf, sizeof(buf), "ID,%u", UART_Address);
     Write((uint8_t*)buf, strlen(buf));
 }
 
@@ -110,7 +114,7 @@ static void Function_TRAIN(char* str, write_func Write)
     // Not currently supported
 }
 
-// "CSETF,1000" - 1000 is in hertz
+// "SETFREQ,1000" - 1000 Hz
 static void Function_SETFREQ(char* str, write_func Write)
 {
     str                          = strtok(NULL, Delims);
@@ -119,6 +123,11 @@ static void Function_SETFREQ(char* str, write_func Write)
         g_sample_every_N_counts = TIM_COUNT_FREQ / requested_sample_freq_hz; // convert val which are hertz to period which is in us
         ChangeSampleFrequency();
     }
+
+    // Echo
+    char buf[30];
+    snprintf(buf, sizeof(buf), "SETFREQ,%u", requested_sample_freq_hz);
+    Write((uint8_t*)buf, strlen(buf));
 }
 
 static void Function_SETSORTTICKS(char* str, write_func Write)
@@ -129,6 +138,11 @@ static void Function_SETSORTTICKS(char* str, write_func Write)
     g_duration_ticks_param = atoi(str);
     str                    = strtok(NULL, Delims);
     g_blind_ticks_param    = atoi(str);
+
+    // Echo
+    char buf[50];
+    snprintf(buf, sizeof(buf), "SETSORTTICKS,%u,%u,%u", g_delay_ticks_param, g_duration_ticks_param, g_blind_ticks_param);
+    Write((uint8_t*)buf, strlen(buf));
 }
 
 static void Function_SETFILTERCOEFF(char* str, write_func Write)
@@ -139,17 +153,27 @@ static void Function_SETFILTERCOEFF(char* str, write_func Write)
     g_hpf_K  = atof(str);
     str      = strtok(NULL, Delims);
     g_lpf2_K = atof(str);
+
+    // Echo
+    char buf[80];
+    snprintf(buf, sizeof(buf), "SETFILTERCOEFF,%.3f,%.3f,%.3f", g_lpf1_K, g_hpf_K, g_lpf2_K);
+    Write((uint8_t*)buf, strlen(buf));
 }
 
 static void Function_SETTHRESHOLD(char* str, write_func Write)
 {
     str         = strtok(NULL, Delims);
     g_threshold = atof(str);
+
+    // Echo
+    char buf[30];
+    snprintf(buf, sizeof(buf), "SETTHRESHOLD,%.1f", g_threshold);
+    Write((uint8_t*)buf, strlen(buf));
 }
 
 static void Function_PING(char* str, write_func Write)
 {
-    Write((uint8_t*)"OK\n", 2);
+    Write((uint8_t*)"PING", 4);
 }
 
 static void Function_GETFREQ(char* str, write_func Write)
@@ -159,7 +183,7 @@ static void Function_GETFREQ(char* str, write_func Write)
     if (g_sample_every_N_counts > 0)
         sample_freq_hz = TIM_COUNT_FREQ / g_sample_every_N_counts;
 
-    snprintf(buf, sizeof(buf), "%u\n", sample_freq_hz);
+    snprintf(buf, sizeof(buf), "%u", sample_freq_hz);
 
     Write((uint8_t*)buf, strlen(buf));
 }
@@ -167,7 +191,7 @@ static void Function_GETFREQ(char* str, write_func Write)
 static void Function_GETSORTTICKS(char* str, write_func Write)
 {
     char buf[50] = {0};
-    snprintf(buf, sizeof(buf), "%u,%u,%u\n", g_delay_ticks_param, g_duration_ticks_param, g_blind_ticks_param);
+    snprintf(buf, sizeof(buf), "%u,%u,%u", g_delay_ticks_param, g_duration_ticks_param, g_blind_ticks_param);
 
     Write((uint8_t*)buf, strlen(buf));
 }
@@ -175,7 +199,7 @@ static void Function_GETSORTTICKS(char* str, write_func Write)
 static void Function_GETFILTERCOEFF(char* str, write_func Write)
 {
     char buf[50] = {0};
-    snprintf(buf, sizeof(buf), "%.3f,%.3f,%.3f\n", g_lpf1_K, g_hpf_K, g_lpf2_K);
+    snprintf(buf, sizeof(buf), "%.3f,%.3f,%.3f", g_lpf1_K, g_hpf_K, g_lpf2_K);
 
     Write((uint8_t*)buf, strlen(buf));
 }
@@ -183,7 +207,7 @@ static void Function_GETFILTERCOEFF(char* str, write_func Write)
 static void Function_GETTHRESHOLD(char* str, write_func Write)
 {
     char buf[10] = {0};
-    snprintf(buf, sizeof(buf), "%.1f\n", g_threshold);
+    snprintf(buf, sizeof(buf), "%.1f", g_threshold);
 
     Write((uint8_t*)buf, strlen(buf));
 }
@@ -194,7 +218,7 @@ static void Function_GETSETTINGS(char* str, write_func Write)
     int  sample_freq_hz = 0;
     if (g_sample_every_N_counts > 0)
         sample_freq_hz = TIM_COUNT_FREQ / g_sample_every_N_counts;
-    snprintf(buf, sizeof(buf), "FREQ,%u\nSORTTICKS,%u,%u,%u\nFILTERCOEFF,%.3f,%.3f,%.3f\nTHRESHOLD,%.1f\n",
+    snprintf(buf, sizeof(buf), "FREQ,%u SORTTICKS,%u,%u,%u FILTERCOEFF,%.3f,%.3f,%.3f THRESHOLD,%.1f",
              sample_freq_hz, g_delay_ticks_param, g_duration_ticks_param, g_blind_ticks_param, g_lpf1_K, g_hpf_K, g_lpf2_K, g_threshold);
 
     Write((uint8_t*)buf, strlen(buf));
@@ -217,8 +241,8 @@ static struct {
     COMMAND(VERG),
     COMMAND(VRBG),
     COMMAND(VRBS),
-    COMMAND(IDST),
-    COMMAND(IDGT),
+    COMMAND(SETID),
+    COMMAND(GETID),
     COMMAND(PING),
     COMMAND(RESET),
     COMMAND(TRAIN),
